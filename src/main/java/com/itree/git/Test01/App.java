b90169757f1a7d2ba7f527e8853a92a0eb9d8954
wen.qing <2848839586@qq.com>
2020-03-03 15:40:43 +0800

    commint

---
+++ src/main/java/com/itree/git/Test01/App.java
@@ -0,0 +1,13 @@
+package com.itree.git.Test01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
